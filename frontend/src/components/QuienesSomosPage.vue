67200030281df3980c5e06f94b71531518af8e4a
tmontess <tomi.montes.7@gmail.com>
2023-11-09 03:19:04 -0300

    quienes somos

--- frontend/src/components/QuienesSomosPage.vue
+++ frontend/src/components/QuienesSomosPage.vue
@@ -1,9 +1,73 @@
 <template>
-    <div>
-   
+  <div>
+    <header class="header">
+      <div class="header-content">
+        <nav class="header-nav">
+          <div class="header-logo">
+            <img src="../assets/imagenes/parsii baner.jpeg" alt="foto-equipos" width="100%">
+          </div>
+          <div class="header-btns" id="show-menu">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <ul class="nav-menu collapse navbar-collapse" id="navbarSupportedContent">
+              <li><a href="/">Inicio</a></li>
+              <li><a href="/contacto">Contacto</a></li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </header>
+
+    <main>
+  <div class="deportes-titles">
+    <h1 style="color: #333;">PARSII</h1>
+  </div>
+  <section class="historia" id="container-aside">
+    <div class="el-club-div1" style="background-color: #f2f2f2; padding: 20px; border-radius: 5px;">
+      <h2 style="color: #333;">Quiénes Somos - Solución integral de gestión para estacionamientos</h2>
+      <div class="text-box">
+        <h3 style="color: #333;">Lo que hacemos</h3>
+        <p style="color: #555; font-size: 16px; line-height: 1.5;">
+          Parsii es el software de gestión para estacionamientos más completo del mercado latinoamericano.
+          Al instante, con Parsii podrás controlar toda la operatoria de tu estacionamiento en tiempo real.
+          Proporcionamos herramientas para facilitar el trabajo de tus empleados y mejorar la experiencia de tus clientes.
+          Mediante nuestra tecnología, mejoramos el control para reducir el fraude y maximizar el rendimiento de tu negocio.
+        </p>
+      </div>
+      <img src="../assets/imagenes/estacionaaaaa.png" alt="foto-club" width="500px" style="margin-top: 20px;">
+    </div>
+  </section>
+
+  <section>
+  <div class="el-club-div2" style="background-color: #f2f2f2; padding: 20px; border-radius: 5px;">
+    <p style="font-size: 16px; line-height: 1.5; color: #333;">
+      Reemplazamos viejos sistemas operativos que son obsoletos, tienen altos costos de reparación y que generan dependencia.
+    </p>
+  </div>
+</section>
+
+      <footer class="footer">
+      <div class="footer-content">
+        <div class="footer-logo">
+          <img src="../assets/imagenes/LOGO.png" alt="escudo" width="80px" height="80px">
+        </div>
+        <div class="footer-info">
+          <h1 class="title-footer">Parsii Estaciona Rapido</h1>
+          <div class="menu-footer">
+            <ul>
+              <li><a href="index.html">Home</a></li>
+              <li><a href="forms-contacto.html">Cómo Llegar</a></li>
+              <li><a href="forms-contacto.html">Contacto</a></li>
+            </ul>
+          </div>
+          <p class="p-footer">
+            <strong>Dirección:</strong> Ing. Enrique Butty 275, C1001 CABA<br>
+            <strong>Teléfono:</strong> 2923 45-8661
+          </p>
+        </div>
+      </div>
+    </footer>
+    </main>
   </div>
 </template>
-
-<style>
-
-</style>
